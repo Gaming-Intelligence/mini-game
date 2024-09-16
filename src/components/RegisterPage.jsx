cebb3d206603a705d6640c829d1979b695191390
SUBHAM SARKAR <127990430+PROPHETCYCO@users.noreply.github.com>
2024-09-16 18:14:09 +0530

    commit

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -65,7 +65,7 @@ const RegisterPage = () => {
 
     return (
         <div className="text-white font-bold p-4">
-            <h1 className='mb-10 text-3xl'>Register Page</h1>
+            <h1 className='mb-10 text-3xl text-blue-600'>Register Page</h1>
 
             {/* Only render the user data if it's available */}
             <div className='shadow-lg rounded-xl shadow-mud p-5 justify-center items-center'>
